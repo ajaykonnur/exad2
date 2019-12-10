ca51cc0cacc917d2bd97840461cc9066a7862746
KoopsTobia <tobias.koops@promos-consult.de>
2019-12-10 12:30:10 +0000

    initial push

---
+++ webapp/controller/main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("promos.exad.EXAD2.controller.main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"promos/exad/EXAD2/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
