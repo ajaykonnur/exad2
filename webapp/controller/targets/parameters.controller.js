0387e82c593a5e7e358c0e7edb3f560854cdebfb
KoopsTobia <tobias.koops@promos-consult.de>
2019-12-17 10:09:02 +0000

    XMLs for every target View +  controller

---
+++ webapp/controller/targets/parameters.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"promos/exad/EXAD2/controller/base.controller"
+], function (baseController) {
+	"use strict";
+
+	return baseController.extend("promos.exad.EXAD2.controller.targets.parameters", {
+		onInit: function () {
+
+		}
+	});
+});
