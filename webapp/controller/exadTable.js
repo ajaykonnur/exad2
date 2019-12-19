172e851afa0eb2e29c9980374fd52fa7cb1f694f
Housseyn Kaouane <housseyn.kaouane@promos-consult.de>
2019-12-19 13:26:04 +0000

    dataBinding

--- webapp/controller/exadTable.js
+++ webapp/controller/exadTable.js
@@ -8,8 +8,20 @@ sap.ui.define([
 		bindColumns: function (oTable, sPath, sModel) {
 			if (oTable === undefined) {
 				Log.error("oTable is undefined.", "bindColumns", this);
+			}else{
+				 oTable.bindColumns(sPath, function(index, context) {  
+	
+				  var columnName = context.getProperty().columnName; 
+				  
+				  return new sap.ui.table.Column({  
+				    label: columnName , 
+				    template: new sap.ui.commons.TextField ({value: { path: "/tableDetails" + index + "/" + columnName } })
+		
+				  });  
+				});  
+				oTable.bindRows('/tableDetails');
+				oTable.setModel(new sap.ui.model.json.JSONModel([{}]) ,'/tableDetails');
 			}
-
 		}
 	};
 
