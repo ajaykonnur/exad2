496d5d2b6f6155be16b1bf98a511bcedbdb393bb
Housseyn Kaouane <housseyn.kaouane@promos-consult.de>
2019-12-20 14:21:41 +0000

    fragmentTabs

--- webapp/controller/exadTable.js
+++ webapp/controller/exadTable.js
@@ -10,17 +10,18 @@ sap.ui.define([
 				Log.error("oTable is undefined.", "bindColumns", this);
 			}else{
 				 oTable.bindColumns(sPath, function(index, context) {  
-	
+		
 				  var columnName = context.getProperty().columnName; 
 				  
 				  return new sap.ui.table.Column({  
 				    label: columnName , 
-				    template: new sap.ui.commons.TextField ({value: { path: "/tableDetails" + index + "/" + columnName } })
+				    template: new sap.ui.commons.TextField ({value: { path: "/tableDetails" + "/" +  index.slice(-1)  + "/" + columnName } }) 
 		
 				  });  
-				});  
-				oTable.bindRows('/tableDetails');
-				oTable.setModel(new sap.ui.model.json.JSONModel([{}]) ,'/tableDetails');
+				}); 
+				
+				oTable.bindRows("/tableDetails");
+				oTable.setModel(new sap.ui.model.json.JSONModel([{}]) ,"/tableDetails");
 			}
 		}
 	};
